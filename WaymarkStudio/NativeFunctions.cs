8877edd9ae29617e349051db11c68cf19d57ddb7
sourpuh <150582765+sourpuh@users.noreply.github.com>
2025-08-05 08:40:33 -0700

    Fix Sig for 7.3

--- WaymarkStudio/NativeFunctions.cs
+++ WaymarkStudio/NativeFunctions.cs
@@ -21,7 +21,7 @@ internal static unsafe class NativeFunctions
     internal static PlacePresetDelegate PlacePreset;
     public static void Initialize()
     {
-        PlaceWaymark = Marshal.GetDelegateForFunctionPointer<PlaceWaymarkDelegate>(Plugin.SigScanner.ScanText("E8 ?? ?? ?? ?? 84 C0 EB 58 44 8B 83 ?? ?? ?? ??"));
+        PlaceWaymark = Marshal.GetDelegateForFunctionPointer<PlaceWaymarkDelegate>(Plugin.SigScanner.ScanText("E8 ?? ?? ?? ?? 84 C0 EB 5C"));
         ClearWaymark = Marshal.GetDelegateForFunctionPointer<ClearWaymarkDelegate>(Plugin.SigScanner.ScanText("E8 ?? ?? ?? ?? EB D8 83 FB 09"));
         ClearWaymarks = Marshal.GetDelegateForFunctionPointer<ClearWaymarksDelegate>(Plugin.SigScanner.ScanText("41 55 48 83 EC 50 4C 8B E9"));
         WaymarkSafety = Marshal.GetDelegateForFunctionPointer<WaymarkSafetyDelegate>(Plugin.SigScanner.ScanText("E8 ?? ?? ?? ?? 84 C0 74 0D B0 05"));
